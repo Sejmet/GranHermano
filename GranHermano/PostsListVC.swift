265a1a9d7fc06390bbfeef7161043b5cdbde496c
Maria Belen Marañon <Sejmet@MacBook-Pro-de-Maria.local>
2016-04-20 15:39:55 -0300

    adding segmentControl

--- GranHermano/PostsListVC.swift
+++ GranHermano/PostsListVC.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class PostsListVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
-
+    
     @IBOutlet weak var tableView: UITableView!
     var imagesArray = [PostObject]()
     var videosArray = [PostObject]()
@@ -21,7 +21,7 @@ class PostsListVC: UIViewController, UITableViewDelegate, UITableViewDataSource 
         
         tableView.estimatedRowHeight = 100.0
         tableView.rowHeight = UITableViewAutomaticDimension
-
+        
         let url = "https://api.iamat.com/atcode/granhermano2015/history/till/now/100?type=sh_img,sh_media"
         
         ApiManager.sharedInstance.fetchAllPostsWith(url) { (postsArray: [PostObject]) in
@@ -39,44 +39,60 @@ class PostsListVC: UIViewController, UITableViewDelegate, UITableViewDataSource 
         }
     }
     
+    @IBAction func segmentedControlValueChanged(sender: UISegmentedControl) {
+        self.tableView.reloadData()
+    }
+    
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         switch self.segmentedControl.selectedSegmentIndex {
         case 0:
             return self.postsArray.count
         case 1:
             return self.imagesArray.count
-        case 2:
-            return self.videosArray.count
         default:
-            return 0
+            return self.videosArray.count
         }
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         switch self.segmentedControl.selectedSegmentIndex {
-        default:
-        
-        let post = self.postsArray[indexPath.row]
-        if (post.event == "sh_img") {
-            let cell = tableView.dequeueReusableCellWithIdentifier("imageCell", forIndexPath:indexPath) as! ImagesPostTVC
+        case 0:
+            let post = self.postsArray[indexPath.row]
+            if (post.event == "sh_img") {
+                let cell = tableView.dequeueReusableCellWithIdentifier("imageCell", forIndexPath:indexPath) as! ImagesPostTVC
+                
+                cell.imageTitleLabel.text = post.data.objectForKey("text")! as? String
+                
+                return cell
+                
+            } else {
+                let cell = tableView.dequeueReusableCellWithIdentifier("videoCell", forIndexPath:indexPath) as! VideoPostTVC
+                
+                let dataInformation = post.data.objectForKey("data")
+                let snippet = dataInformation?.objectForKey("snippet")
+                cell.videoTitleLabel.text = snippet?.objectForKey("title")! as? String
+                cell.videoDescriptionLabel.text = snippet?.objectForKey("description")! as? String
+                
+                return cell
+            }
             
+        case 1:
+            let post = self.imagesArray[indexPath.row]
+            let cell = tableView.dequeueReusableCellWithIdentifier("imageCell", forIndexPath:indexPath) as! ImagesPostTVC
             cell.imageTitleLabel.text = post.data.objectForKey("text")! as? String
             
             return cell
-            
-        } else {
+        default:
+            let post = self.videosArray[indexPath.row]
             let cell = tableView.dequeueReusableCellWithIdentifier("videoCell", forIndexPath:indexPath) as! VideoPostTVC
             
             let dataInformation = post.data.objectForKey("data")
             let snippet = dataInformation?.objectForKey("snippet")
+
             cell.videoTitleLabel.text = snippet?.objectForKey("title")! as? String
             cell.videoDescriptionLabel.text = snippet?.objectForKey("description")! as? String
             
             return cell
         }
-        
-        
-        }
     }
-
 }
